f87d5f059ba71533f9db65ecb43aac2e05e7771c
ankitkothe2119 <135228960+ankitkothe2119@users.noreply.github.com>
2023-11-08 16:28:27 +0000

    add mongoose db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
-import { express } from "express";
-import { bodyParser } from "body-parser";
-import { mongoose } from "mongoose";
-import { Cors } from "cors";
-import dotenv from "dotenv";
-import { multer } from "multer";
+import  express  from "express";
+import bodyParser  from "body-parser";
+import  mongoose  from "mongoose";
+import cors  from "cors";
+import  multer  from "multer";
 import helmet from "helmet";
-import { morgan } from "morgan";
-import { path } from "path";
+import  morgan  from "morgan";
+import  path  from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import  createBrotliCompress  from "zlib";
 
 /* configrations */
 const __filename =fileURLToPath(import.meta.url);
@@ -17,7 +18,31 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
-app.use(bodyParser.json({limit: "30mb",etxnded: true}));
+app.use(morgan("common"))
+app.use(bodyParser.json({limit: "30mb",extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use(cors());
 app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+
+/* flie storage */
+const storage =multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null,"public/assets")
+    },
+    filename: function (req, file, cb){
+        cb(null, file.originalname)
+    }
+});
+const upload =multer({storage})
+/* Mongoose setup*/
+const PORT =process.env.PORT || 6001;
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+.then(()=>{
+    app.listen(PORT,()=>console.log(`Server port: ${PORT}`))
+})
+.catch((error)=>console.log(`${error}  Did not connect`))
