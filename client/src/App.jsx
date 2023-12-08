2a234d61fb28882c208a0a1f2abf45a219e4467a
ankitkothe2119 <135228960+ankitkothe2119@users.noreply.github.com>
2023-12-08 17:41:48 +0000

    navbar creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from 'scenes/homePage';
-import LoginPage from 'scenes/loginPage';
-import ProfilePage from 'scenes/profilePage';
+import HomePage from './scenes/homePage/index.jsx';
+import LoginPage from './scenes/loginPage/index.jsx';
+import ProfilePage from './scenes/profilePage/index.jsx';
 // import Navbar from 'scenes/navbar';
 import useMemo from "react";
 import { useSelector } from 'react-redux';
